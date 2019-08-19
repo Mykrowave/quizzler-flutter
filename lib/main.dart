47768b63802dda8279064eae5581d8f7ab359abd
Mike Parker <mikeparker@crescerance.com>
2019-08-18 22:20:44 -0400

    loopthroughquestions

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> _scoreKeeper = [];
+  List<String> _questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> _correctAnswers = [false, true, true];
+
+  int _currentQuestionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                _questions[_currentQuestionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +72,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  markScore(true, _correctAnswers[_currentQuestionIndex]);
+                  _currentQuestionIndex =
+                      (_currentQuestionIndex >= _questions.length - 1)
+                          ? 0
+                          : _currentQuestionIndex + 1;
+                });
               },
             ),
           ),
@@ -79,14 +96,31 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  markScore(false, _correctAnswers[_currentQuestionIndex]);
+                  _currentQuestionIndex =
+                      (_currentQuestionIndex >= _questions.length - 1)
+                          ? 0
+                          : _currentQuestionIndex + 1;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          child: Row(
+            children: _scoreKeeper,
+          ),
+        )
       ],
     );
+  }
+
+  void markScore(bool answer, bool correctAnswer) {
+    if (answer == correctAnswer)
+      _scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+    else
+      _scoreKeeper.add(Icon(Icons.close, color: Colors.red));
   }
 }
 
